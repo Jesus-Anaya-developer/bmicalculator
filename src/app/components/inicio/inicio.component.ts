c375135b1bf7f51ee1234f20c8690de9ae0e3262
Jesus Anaya <jesus.anaya.developer@gmail.com>
2023-05-20 22:39:54 -0600

    app completada

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent {
+
+  edad: number = 25;
+  peso: number = 60;
+  altura: number = 170;
+  sexo: string = 'Masculino';
+
+  constructor(private router: Router) { }
+
+  cambiarAltura(event: any) {
+    this.altura = event.target.value;
+  }
+
+  Masculino() {
+    this.sexo = 'Masculino';
+  }
+
+  Femenino() {
+    this.sexo = 'Femenino';
+  }
+
+  calcularBMI() {
+    const bmi = this.peso / Math.pow(this.altura / 100, 2);
+
+    this.router.navigate(['/resultado', bmi.toFixed(1)]);
+  }
 
 }
--- src/app/components/resultado/resultado.component.ts
+++ src/app/components/resultado/resultado.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-resultado',
   templateUrl: './resultado.component.html',
   styleUrls: ['./resultado.component.scss']
 })
-export class ResultadoComponent {
+export class ResultadoComponent implements OnInit {
+
+  bmi: number = 0;
+  resultado: string = '';
+  interpretacion: string = '';
+
+  constructor(private route: ActivatedRoute) {
+    this.bmi = +route.snapshot.paramMap.get('valor')!;
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getResultado();
+  }
+
+  getResultado() {
+
+    if (this.bmi < 15) {
+      this.resultado = 'Muy bajo peso';
+      this.interpretacion = 'Tienes un peso corporal mas bajo de lo normal. Puedes comer un poco mas';
+    } else if (this.bmi >= 15 && this.bmi < 16) {
+      this.resultado = 'Peso muy bajo';
+      this.interpretacion = 'Tienes un peso corporal mas bajo de lo normal. Puedes comer un poco mas';
+    } else if (this.bmi >= 16 && this.bmi < 18.5) {
+      this.resultado = 'Peso insuficiente';
+      this.interpretacion = 'Tienes un peso corporal mas bajo de lo normal. Puedes comer un poco mas';
+    } else if (this.bmi >= 18.5 && this.bmi < 25) {
+      this.resultado = 'Normal (peso saludable)';
+      this.interpretacion = 'Tienes un peso corporal normal. ¡Buen trabajo!';
+    } else if (this.bmi >= 25 && this.bmi < 30) {
+      this.resultado = 'Sobrepeso';
+      this.interpretacion = 'Tienes un peso corporal superior al normal. Intenta hacer mas ejercicio';
+    } else if (this.bmi >= 30 && this.bmi < 35) {
+      this.resultado = 'Moderadamente obeso';
+      this.interpretacion = 'Tienes un peso corporal superior al normal. Intenta hacer mas ejercicio';
+    } else if (this.bmi >= 35 && this.bmi < 40) {
+      this.resultado = 'Obesidad severa';
+      this.interpretacion = 'Tienes un peso corporal superior al normal. Intenta hacer mas ejercicio';
+    } else if (this.bmi >= 40) {
+      this.resultado = 'Obesidad muy grave';
+      this.interpretacion = 'Tienes un peso corporal superior al normal. Intenta hacer mas ejercicio';
+    }
+
+  }
 
 }
